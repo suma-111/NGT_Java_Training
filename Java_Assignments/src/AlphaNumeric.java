6ca9fca693572821355dd30b57dc6b741d9aec3e
suma-111 <105193804+suma-111@users.noreply.github.com>
2022-05-09 12:04:27 +0530

    Update AlphaNumeric.java

--- Java_Assignments/src/AlphaNumeric.java
+++ Java_Assignments/src/AlphaNumeric.java
@@ -1,6 +1,28 @@
 
 public class AlphaNumeric 
 {
+	
+	public static void main(String[] args) {
+		int c=0, d=9;
+		String str = "Get 1 The 22 Future 333 You 444 Want";
+		str=str.toLowerCase();
+	
+		for(int i=0; i<str.length(); i++) {
+			
+			if(str.charAt(i)>=(char)97 && str.charAt(i)<=(char)122) {
+				//No action
+			    //System.out.print(str.charAt(i));
+			}
+			else if(str.charAt(i)==' '){
+				System.out.println("");
+			}
+
+			else {
+				System.out.print(str.charAt(i));
+			}			
+		}		
+	}
+
 
 	
 }
